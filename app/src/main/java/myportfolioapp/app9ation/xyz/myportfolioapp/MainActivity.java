1b665dc3afd1ab951e59d887168db770f31473ce
bhupendra11 <bhupendrashekhawat1108@gmail.com>
2015-11-21 04:49:10 +0530

    Some Cleanup in first iteration and configure String resources

--- app/src/main/java/myportfolioapp/app9ation/xyz/myportfolioapp/MainActivity.java
+++ app/src/main/java/myportfolioapp/app9ation/xyz/myportfolioapp/MainActivity.java
@@ -27,32 +27,11 @@ public class MainActivity extends AppCompatActivity {
         // 1) Possibly check for instance of first
         Button b = (Button)v;
         String buttonText = b.getText().toString();
-        String display = "The button will launch " + buttonText.toString().toLowerCase();
+        String display = "The button will launch " + buttonText.toLowerCase();
         Toast t = Toast.makeText(this,display,Toast.LENGTH_SHORT);
         t.show();
 
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
